dfb39c1308002417c6380df81649edbd56a9191d
owonikoko Ayomide <donaykoko@yahoo.com>
2024-02-09 12:33:27 +0100

    Update getid.php

--- getid.php
+++ getid.php
@@ -7,7 +7,7 @@ if (isset($_GET['product_id'])) {
   $result = mysqli_query($connection, $sql);
   $row = mysqli_fetch_array($result);
   $view = $row['views']+1;
-  $update = mysqli_query($connection,"UPDATE link SET views='$view' WHERE id  = '$product_id' ");
+  $update = mysqli_query($connection,"UPDATE link SET views='$view' WHERE id  = '$product_id' "); 
   if ($update) {
     // $response = "watched";
     echo $view;
