d685f3e5923be55578f6117e1e8302ff1ab04657
owonikoko Ayomide <donaykoko@yahoo.com>
2024-03-13 12:01:43 +0100

    Update getid.php

--- getid.php
+++ getid.php
@@ -4,7 +4,7 @@ include('config.php');
 if (isset($_GET['product_id'])) {
   $product_id = $_GET['product_id'];
   $sql = "SELECT * FROM link  WHERE id  = '$product_id'";
-  $result = mysqli_query($connection, $sql);
+  $result = mysqli_query($connection, $sql); 
   $row = mysqli_fetch_array($result);
   $view = $row['views']+1;
   $update = mysqli_query($connection,"UPDATE link SET views='$view' WHERE id  = '$product_id' "); 
