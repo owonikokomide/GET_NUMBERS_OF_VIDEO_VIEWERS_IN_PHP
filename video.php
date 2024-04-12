b2fbc1f1f52fd7f360379f8183eee988815dfe5b
owonikoko Ayomide <donaykoko@yahoo.com>
2024-04-12 11:32:20 +0100

    Update video.php

--- video.php
+++ video.php
@@ -6,7 +6,7 @@ if(isset($_POST['play'])){
     $select_query = mysqli_query($connection, $select);
     $row = mysqli_fetch_array($select_query);
     $r = $row['views']+1;
-$update = mysqli_query($connection,"UPDATE link SET views ='$r' WHERE id ='$id'");
+$update = mysqli_query($connection,"UPDATE link SET views ='$r' WHERE id ='$id'"); 
 if($update){
 //  echo "succesful"; 
 }
